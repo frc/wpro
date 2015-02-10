ef3666ba65fbc69fe4b14e8b97231f43bc5e11c9
Ryan Parlee <rparlee@flyinghippo.com>
2015-02-10 10:55:50 -0600

    updated version to 1.1 and updated authorship

--- wpro.php
+++ wpro.php
@@ -1,11 +1,11 @@
 <?php
 /**
 Plugin Name: WP Read-Only
-Plugin URI: http://wordpress.org/extend/plugins/wpro/
+Plugin URI: https://github.com/parleer/wpro
 Description: Plugin for running your Wordpress site without Write Access to the web directory. Amazon S3 is used for uploads/binary storage. This plugin was made with cluster/load balancing server setups in mind - where you do not want your WordPress to write anything to the local web directory.
-Version: 1.0
-Author: alfreddatakillen
-Author URI: http://nurd.nu/
+Version: 1.1
+Author: parleer, alfreddatakillen
+Author URI: https://github.com/parleer
 License: GPLv2
  */
 
